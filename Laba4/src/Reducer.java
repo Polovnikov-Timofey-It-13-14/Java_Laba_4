f15a888e5ddfe22b23654e58e6964ccf94b7f5d2
Polovnikov-Timofey-It-13-14 <polovnikovtu999@gmail.com>
2025-10-27 08:26:44 +0500

    Update Reducer.java

--- Laba4/src/Reducer.java
+++ Laba4/src/Reducer.java
@@ -1,6 +1,7 @@
 import java.util.List;
 
 public class Reducer {
+    //Метод для сокращения
     public static <T> T reduce(List<T> list, Reduce<T> reducer, T initialValue) {
         T result = initialValue;
         for (T item : list) {
