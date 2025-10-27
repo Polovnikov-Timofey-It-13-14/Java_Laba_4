087a5be7cb138d65ad11a0901899f5eb95770fdb
Polovnikov-Timofey-It-13-14 <polovnikovtu999@gmail.com>
2025-10-27 08:25:54 +0500

    Update Transform.java

--- Laba4/src/Transform.java
+++ Laba4/src/Transform.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Transform {
+    //Метод для трансформации списков
     public static <T, P> List<P> transform(List<T> list, Function<T, P> function) {
         List<P> result = new ArrayList<>();
         for (T item : list) {
