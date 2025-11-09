3fd36cd712322d50920d581a41151681ea190a35
Polovnikov-Timofey-It-13-14 <polovnikovtu999@gmail.com>
2025-11-10 02:04:49 +0500

    Update Filter.java

--- Laba4/src/Filter.java
+++ Laba4/src/Filter.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Filter {
-    //Метод для фильтрации списков
+    //Метод для фильтрации
     public static <T> List<T> filter(List<T> list, Predicate<T> predicate) {
         List<T> result = new ArrayList<>();
         for (T item : list) {
